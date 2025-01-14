c0019b1133b2dd6bad0e69317e9f655c866cfc06
slh cachyos <70451183+slh-git@users.noreply.github.com>
2025-01-14 15:34:25 -0500

    Init

---
+++ 49-Group_Anagrams/group_anagram.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+
+# Count the letter combo of each word, using the count as a key insert it into a defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:  # noqa: F821
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += (
+                    1  # Ascii # of letter - ascii of a to get the index
+                )
+            res[tuple(count)].append(s)  # tuple cause cant take list
+        return list(res.values())
---
+++ cheatsheet.md
@@ -0,0 +1,24 @@
+# Cheatsheet for the leet Python
+
+<!--toc:start-->
+
+- [Cheatsheet for the leet Python](#cheatsheet-for-the-leet-python)
+  - [List](#list)
+    - [Uncommon maneuvers](#uncommon-maneuvers)
+  - [Alphabet](#alphabet) - [Alphabet to index](#alphabet-to-index)
+  <!--toc:end-->
+
+## List
+
+### Uncommon maneuvers
+
+- List repetition: `[0] * 3 -> [0, 0, 0]`
+
+## Alphabet
+
+### Alphabet to index
+
+Ascii # of letter - ascii of a to get the index
+
+- `ord(char) - ord('a')`
+  - ord: The ord() function returns the number representing the unicode code of a specified character.
