36c41bd46c6fb6ab9cf9171cbedd186a4eefdd99
slh cachyos <70451183+slh-git@users.noreply.github.com>
2025-03-27 22:42:02 -0400

    Daily: Remove nth from end of linklist

---
+++ 19-Remove_Nth_Node_from_LinkList/remove_nth_from_end.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+#
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        first = dummy
+        second = dummy
+
+        for _ in range(n + 1):
+            first = first.next
+
+        # If first is None, we're removing the head
+        if not first:
+            return dummy.next.next
+
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+        return dummy.next
+
+
+# NOTE: O(n) time complexity, O(1) space complexity n=linklist length
+#
