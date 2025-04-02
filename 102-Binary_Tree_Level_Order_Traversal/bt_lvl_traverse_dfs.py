2f1aef25e64d00f893e0a7bacfa10f066bf62650
slh cachyos <70451183+slh-git@users.noreply.github.com>
2025-04-01 23:26:27 -0400

    Daily

--- 102-Binary_Tree_Level_Order_Traversal/bt_lvl_traverse_dfs.py
+++ 102-Binary_Tree_Level_Order_Traversal/bt_lvl_traverse_dfs.py
@@ -13,30 +13,6 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         out = []
 
-        def dfs(node, level):
-            if not node:
-                return None
-            if len(out) == level:
-                out.append([])
-            out[level].append(node.val)
-            dfs(node.left, level + 1)
-            dfs(node.right, level + 1)
-
-        dfs(root, 0)
-
-        return out
-
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        out = []
-
         def dfs(root, lvl):
             if not root:
                 return
